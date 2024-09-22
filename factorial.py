4ec0b654b3c38626f003c964a5966e13d71699b4
unknown <sagarazm999@gmail.com>
2024-09-22 09:18:58 +0530

    factorial program

---
+++ factorial.py
@@ -0,0 +1,10 @@
+
+    #factorial
+def factorial(n):
+    if(n==0 or n==1):
+        return 1
+    else:
+        return n * factorial(n-1)
+n=int(input("Input a number to compute the factiorial:"))
+print(factorial(n))
+
